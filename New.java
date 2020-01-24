1ce4f690fdeb4de34dfda727b9f4420f620ea541
bhanusaia <bhanusai.adabala@gmail.com>
2020-01-24 15:57:43 +0000

    Latest commit after reset

---
+++ New.java
@@ -0,0 +1,8 @@
+ import java.lang.*;
+public class New{
+ public static void main(String args[])
+{ 
+  int a=10;
+  System.out.println("a"+a);
+}
+}
---
+++ Polymor.java
@@ -0,0 +1,26 @@
+import java.lang.*;
+public Polymor
+{
+ public int add(int x, int y)
+   {
+     return(x+y);
+    }
+ public int add(int x, int y, int z)
+    {
+      return(x+y+z);
+   }
+  public int add(double x, double y)
+  {
+     return(x+y);
+  }
+}
+public  static void main(String args[])
+  {
+      Polymor p=new Polymor();
+    System.out.println(p.add(10,20));
+    System.out.println(p.add(10,20,30));
+    System.out.println(p.add(150,250));
+ }
+
+
+
